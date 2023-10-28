aa515bcb3cf25771938272922487bdbffd63e9d8
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-10-28 12:00:25 -0500

    hago conectividad mediante base de datos y agrego pestaña carrito (sin funcionalidad)

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -18,14 +18,8 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Conócenos</a></li>
-        <li>
-          <a>Cursos</a>
-          <ul class="p-2">
-            <li><a>c 1</a></li>
-            <li><a>c 2</a></li>
-          </ul>
-        </li>
+        <li><a href="/">Conócenos</a></li>
+        <li><a>Cursos</a></li>
 
       </ul>
     </div>
@@ -36,15 +30,6 @@
       <li><a href="./">Conócenos</a></li>
       <li><a>Cursos</a></li>
       <li><a href="./registro">Regístrate</a></li>
-      {{-- <li tabindex="0">
-        <details>
-          <summary>Cursos</summary>
-          <ul class="p-2">
-            <li><a>c 1</a></li>
-            <li><a>c 2</a></li>
-          </ul>
-        </details>
-      </li> --}}
 
     </ul>
   </div>
@@ -62,7 +47,7 @@
           <span class="font-bold text-lg">0 Items</span>
           <span class="text-info">Subtotal: $0</span>
           <div class="card-actions">
-            <button class="btn btn-primary btn-block">View cart</button>
+            <a href="/carrito" class="btn btn-primary btn-block">Ver Carrito</a>
           </div>
         </div>
       </div>
@@ -89,7 +74,7 @@
 
 
 {{-- aqui iran los cursos, al costado izquierdo tendrá un aside para filtrar por categoria y en el derecho los cursos mostrados en grid --}}
-<div class="bg-gray-100 font-sans ">
+<div class="font-sans max-w-[1200px] m-auto">
     <div class="flex flex-col md:flex-row min-h-[80vh]">
         <!-- Aside (Barra lateral izquierda) -->
         <aside class="max-w-full md:w-1/4 bg-gray-200 p-4 max-h-[300px] m-4">
@@ -114,22 +99,23 @@
         <!-- Contenido principal (Productos en grilla) -->
         <div class="w-full md:w-3/4 p-4 min-h-max">
             <h1 class="text-2xl font-semibold mb-4">Cursos</h1>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
                
                 
 
                 @foreach ($cursos as $curso)
-                    <div class="card bg-base-100 shadow-xl">
-                        <figure><img src="{{ $curso['imagen'] }}" alt="{{ $curso['nombre'] }}" /></figure>
+                    <div class="card bg-base-100 shadow-xl max-w-[260px] hover:scale-[1.01] ease-in duration-300">
+                        <figure><img src="{{ $curso->URL_imagen }}" alt="{{ $curso->Nombre_curso }}" /></figure>
                         <div class="card-body">
-                            <h2 class="card-title">{{ $curso['nombre'] }}</h2>
-                            <p>{{ $curso['descripcion'] }}</p>
+                            <h2 class="card-title">{{ $curso->Nombre_curso }}</h2>
+                            <p>{{ $curso->Descripción }}</p>
                             <div class="card-actions justify-end">
                                 <button class="btn btn-primary">Comprar</button>
                             </div>
                         </div>
                     </div>
-                  @endforeach
+                @endforeach
+                
 
 
             </div>
@@ -141,7 +127,7 @@
 <footer class="footer p-10 bg-neutral text-neutral-content">
     <aside>
       <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-      <p>ACME Industries Ltd.<br/>Providing reliable tech since 2023</p>
+      <p>Asociación grupo 2<br/>Brindado servicios desde 2023-2</p>
     </aside> 
     <nav>
       <header class="footer-title">Social</header> 
--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,24 @@ Route::get('/', function () {
     return view('landing');
 });
 
-Route::get('/cursos', function () {
-    return view('cursos');
+// Carrito
+Route::get('/carrito', function () {
+    return view('carrito');
 });
-
-Route::get('/registro', function () {
-    return view('registro');
-});
-
-
 
 // CRUD de cursos 
-Route::get('/cursos', [CursoController::class, 'index']);
-Route::get('/cursos/create', [CursoController::class, 'create'])->name('cursos.create');
-Route::post('/cursos', [CursoController::class, 'store'])->name('cursos.store');
-Route::get('/cursos/{idCurso}', [CursoController::class, 'show'])->name('cursos.show');
-Route::get('/cursos/{idCurso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
-Route::put('/cursos/{idCurso}', [CursoController::class, 'update'])->name('cursos.update');
-Route::delete('/cursos/{idCurso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+Route::controller(CursoController::class)->group(function(){
+
+    Route::get('/cursos', 'index');
+    Route::get('/cursos/create', 'create')->name('cursos.create');
+    Route::post('/cursos', 'store')->name('cursos.store');
+    Route::get('/cursos/{idCurso}', 'show')->name('cursos.show');
+    Route::get('/cursos/{idCurso}/edit', 'edit')->name('cursos.edit');
+    Route::put('/cursos/{idCurso}', 'update')->name('cursos.update');
+    Route::delete('/cursos/{idCurso}', 'destroy')->name('cursos.destroy');
+    
+});
+
 
 // CRUD de usuarios
 
