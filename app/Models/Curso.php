f50b38e3daf22b007122deed6da511545e70e97f
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-10-28 23:10:48 -0500

    modelo Cursos no usa timestams asi que lo desactivo

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -10,5 +10,6 @@ class Curso extends Model
     use HasFactory;
     protected $table = 'cursos'; // no es necesario si la tabla es el plural del modelo, de preferencia definir el nombre de la tabla
 
+    public $timestamps = false; // si no se usa el created_at y updated_at en la tabla, se debe desactivar
 
 }
