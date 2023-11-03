e16a16ed7a9d308a35f893dfe68847df9ff974cc
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-03 17:48:21 -0500

    permito la subida de fechas de edicion y modificacion de cursos

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -10,7 +10,7 @@ class Curso extends Model
     use HasFactory;
     protected $table = 'cursos'; // no es necesario si la tabla es el plural del modelo, DE PREFERENCIA definir el nombre de la tabla
 
-    public $timestamps = false; // si no se usa el created_at y updated_at en la tabla, se debe desactivar
+    //public $timestamps = false; // si no se usa el created_at y updated_at en la tabla, se debe desactivar
 
     protected $fillable = ['nombre', 'descripcion', 'categoria', 'precio', 'url_imagen']; 
     // se debe definir los campos que se van a llenar en la tabla para que no haya sql injection o errores de seguridad
