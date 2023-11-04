983e17b6ca9c9fe80194a00df48e47a78c41eaff
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-03 21:05:46 -0500

    añado una tabla que muestra los temas-videos del curso

--- resources/views/detalle_curso.blade.php
+++ resources/views/detalle_curso.blade.php
@@ -110,6 +110,29 @@
             </div>
         </div>
     </div>
+
+    {{-- tabla de los nombres video que se brindarán: --}}
+    <div class="mt-4 flex items-center justify-center">
+      <div class="w-full max-w-screen-md p-4 bg-white rounded-lg shadow-lg">
+        <table class="w-full border-collapse border border-gray-300">
+          <thead>
+            <tr>
+              <th class="border border-gray-300 px-4 py-2">N°</th>
+              <th class="border border-gray-300 px-4 py-2">Contenido</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($videos as $video)
+              <tr>
+                <td class="border border-gray-300 px-4 py-2">{{$video->orden}}</td>
+                <td class="border border-gray-300 px-4 py-2">{{$video->nombre}}</td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+    
   
 </body>
 </html>
