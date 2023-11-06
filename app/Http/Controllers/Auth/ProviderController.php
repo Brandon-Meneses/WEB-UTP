9b8d8cd1e351d6ee4026ee207e1c758c3681fec3
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-06 01:18:35 -0500

    configuraciones para autenticacion - modelo y controlador

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -16,23 +17,19 @@ class ProviderController extends Controller
 
     public function callback($provider)
     {
-        // Obtenemos los datos del usuario
-        $social_user = Socialite::driver($provider)->user();
-
-        dd($social_user);
-
-        // Comprobamos si el usuario ya existe
-        if ($user = User::where('email', $social_user->email)->first()) {
-            return $this->authAndRedirect($user); // Login y redirección
-        } else {
-            // En caso de que no exista creamos un nuevo usuario con sus datos.
-            $user = User::create([
-                'name' => $social_user->name,
-                'email' => $social_user->email,
-                //'avatar' => $social_user->avatar,
-            ]);
-
-            return $this->authAndRedirect($user); // Login y redirección
-        }
+        $SocialUser = Socialite::driver('github')->user();
+ 
+        $user = User::updateOrCreate([
+            'provider_id' => $SocialUser->id,
+            'provider' => $provider
+        ], [
+            'name' => $SocialUser->name,
+            'email' => $SocialUser->email,
+            'provider_token' => $SocialUser->token,
+        ]);
+     
+        Auth::login($user);
+     
+        return redirect('/dashboard');
     }
 }
