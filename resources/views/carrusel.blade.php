c89aff5d7607698b109d4d55615b3945e180221e
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2023-11-07 17:12:07 -0500

    Se visualizan los cursos de manera dinamica comprados y alojados en la base de datos

--- resources/views/carrusel.blade.php
+++ resources/views/carrusel.blade.php
@@ -1,26 +1,17 @@
 
   <div class="my-8  flex items-center justify-center">
+    <div class="max-w-[950px] carousel carousel-center rounded-box">
+        @foreach ($cursos as $curso)
+            @foreach ($curso as $item)
+                <div class="carousel-item">
+                    <img src="{{ $item->url_imagen }}" alt="{{ $item->nombre }}" />
+                </div>
+            @endforeach
+        @endforeach
+    </div>
+</div>
 
-    <div class=" max-w-[950px] carousel carousel-center rounded-box ">
-      <div class="carousel-item">
-        <img src="https://i.postimg.cc/wBZ37HJZ/sql.jpg"  alt="Pizza" />
-      </div>
-      <div class="carousel-item ">
-        <img src="https://i.postimg.cc/jS8DYyjs/movil-React.jpg"  alt="Pizza" />
-      </div>
-      <div class="carousel-item ">
-        <img src="https://i.postimg.cc/brjJ9TmR/web.jpg"  alt="Pizza" />
-      </div>
-      <div class="carousel-item ">
-        <img src="https://i.postimg.cc/j5f2f945/python.jpg"  alt="Pizza" />
-      </div>
-      <div class="carousel-item ">
-        <img src="https://i.postimg.cc/d3m0HyXc/node.jpg"  alt="Pizza" />
-    </div>
-    <div class="carousel-item ">
-      <img src="https://i.postimg.cc/W4hhNtD0/java.jpg"  alt="Pizza" />
-    </div>
-  </div>
+
 
   {{-- {{dd($cursos)}} --}}
   
