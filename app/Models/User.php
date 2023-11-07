985dbdc28f5ed506f56efdf352a044eddc00c0af
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-06 19:24:08 -0500

    se solicita confirmar el email para poder ser registrado en la BD

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail // para que el usuario tenga que verificar su email
 {
     use HasApiTokens, HasFactory, Notifiable;
 
