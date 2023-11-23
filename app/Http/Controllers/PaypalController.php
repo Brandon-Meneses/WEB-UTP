09e47c565d7f3cbe81f0896fad9f38ed3cae12e8
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-23 00:19:23 -0500

    al comprar el curso ahora se puede ver en el dashboard, se añade a la tabla compra

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Curso;
+use App\Models\Compra;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
@@ -14,8 +16,13 @@ class PaypalController extends Controller
     {
         $curso = Curso::find($request->idCurso)->toArray();
         $usuario = auth()->user()->toArray();
-        
+
         //dd($curso, $usuario);
+
+        //si el usuario ya ha comprado el curso (si existe en la tabla compras) no se puede volver a comprar
+        if(Compra::where('id_usuario', $usuario['id'])->where('id_curso', $curso['id'])->exists()) {
+            return redirect()->route('dashboard')->with('mensaje', 'Ya has comprado este curso');
+        }
 
         $provider = new PayPalClient;
         $provider->setApiCredentials(config('paypal'));
@@ -29,7 +36,8 @@ class PaypalController extends Controller
                 'landing_page' => 'BILLING',
                 'shipping_preference' => 'NO_SHIPPING',
                 'user_action' => 'PAY_NOW',
-                'return_url' => route('paypal.success', ['idCurso' => $curso['id']]), // aqui le paso el id del curso para que lo reciba en el controlador
+                // Aqui podemos pasar parametros adicionales:
+                'return_url' => route('paypal.success', ['idCurso' => $curso['id'], 'idUsuario' => $usuario['id']]),
                 'cancel_url' => route('paypal.cancel'),
             ],
             'purchase_units' => [
@@ -51,7 +59,7 @@ class PaypalController extends Controller
                 }
             }
         } else {
-            return redirect()->route('paypal.cancel')->with('error', 'Error al procesar el pago');
+            return redirect()->route('paypal.cancel')->with('mensaje', 'Error al procesar el pago');
         }
 
     }
@@ -66,12 +74,25 @@ class PaypalController extends Controller
 
         if(isset($response['status']) && $response['status'] == 'COMPLETED') {
             $curso = Curso::find($request->idCurso);
+            $usuario = User::find($request->idUsuario);
 
+            //se agrega el curso comprado al usuario en la tabla Compras
+            /*                                               USUARIO, CURSO
+            INSERT INTO compras (id_usuario, id_curso) VALUES (1, 1),*/
+            $compra = new Compra([
+                'id_usuario' => $usuario->id,
+                'id_curso' => $curso->id,
+            ]);
+            $compra->save();
+
+            /*
             return response()->json([
                 'status' => 'success',
                 'message' => 'Pago realizado con éxito',
                 'curso' => $curso,
-            ]);
+                'usuario' => $usuario,
+            ]); */
+            return redirect()->route('dashboard')->with('mensaje', 'Pago realizado con éxito');
         } else {
             return redirect()->route('paypal.cancel');
         }
